0251555ab1c700ba79d62bf3925beef76969529a
Axel Cawe <Cawe321Mods@gmail.com>
2024-10-11 20:27:50 +0800

    Update DefaultClanTierModelPatch_CompanionLimit.cs
    Uses GetCompanionLimit instead of the other function GetCompanionLimitFromTier as harmony does not respect it's return value when other mods patches GetCompanionLimit

--- BannerlordExpanded.CompanionExpanded/AdditionalCompanionSlots/Patches/DefaultClanTierModelPatch_CompanionLimit.cs
+++ BannerlordExpanded.CompanionExpanded/AdditionalCompanionSlots/Patches/DefaultClanTierModelPatch_CompanionLimit.cs
@@ -1,18 +1,19 @@
 ﻿using BannerlordExpanded.CompanionExpanded.Settings;
 using HarmonyLib;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.GameComponents;
 
 namespace BannerlordExpanded.CompanionExpanded.AdditionalCompanionSlots.Patches
 {
     [HarmonyPatchCategory("AdditionalCompanionSlots")]
-    [HarmonyPatch(typeof(DefaultClanTierModel), "GetCompanionLimitFromTier")]
+    [HarmonyPatch(typeof(DefaultClanTierModel), "GetCompanionLimit")]
     public static class DefaultClanTierModelPatch_CompanionLimit
     {
 
         [HarmonyPostfix]
-        static void Postfix(ref int __result, int clanTier)
+        static void Postfix(ref int __result, Clan clan)
         {
-            __result += clanTier * MCMSettings.Instance.AdditionalCompanionsPerClanTier;
+            __result += clan.Tier * MCMSettings.Instance.AdditionalCompanionsPerClanTier;
         }
     }
 }
