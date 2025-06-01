7947a78ed296f6552e1d1b7632cfb21494e87aa9
Snow Eilfel <119555523+AxelCawe@users.noreply.github.com>
2025-06-01 21:09:25 +0800

    Workaround logic as game does not recognize companions as troops

---
+++ BannerlordExpanded.CompanionExpanded/Misc/Patches/TroopLostCountPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Siege;
+
+namespace BannerlordExpanded.SpousesExpanded.PregnancyAge.Patches
+{
+
+    [HarmonyPatch(typeof(DefaultTroopSacrificeModel), "GetLostTroopCount", typeof(MobileParty), typeof(SiegeEvent))]
+    public static class TroopLostCountPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(DefaultTroopSacrificeModel __instance, MobileParty party, ref int __result)
+        {
+            if (__result > 0 && party.MemberRoster.TotalManCount > __result && party.MemberRoster.TotalRegulars < __result)
+            {
+                __result = party.MemberRoster.TotalRegulars;
+            }
+        }
+
+
+    }
+}
